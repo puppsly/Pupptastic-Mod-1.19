db9280e4d8177f148dd5aeb1a40c6e632164cb33
Puppsly <mitziederren@gmail.com>
2023-04-16 08:51:46 -0500

    actual first commit

---
+++ src/main/java/net/puppsly/pupptasticmod/PupptasticMod.java
@@ -0,0 +1,14 @@
+package net.puppsly.pupptasticmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PupptasticMod implements ModInitializer {
+	public static final String MOD_ID = "pupptasticmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/puppsly/pupptasticmod/PupptasticModClient.java
@@ -0,0 +1,10 @@
+package net.puppsly.pupptasticmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PupptasticModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
